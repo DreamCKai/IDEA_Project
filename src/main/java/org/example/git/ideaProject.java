dec3e2573b17056e0874c97a4b75ac4d6518da4e
DreamCKai <ckailong@yeah.net>
2023-09-20 19:06:59 +0800

    第二次提交

--- src/main/java/org/example/git/ideaProject.java
+++ src/main/java/org/example/git/ideaProject.java
@@ -3,5 +3,6 @@ package org.example.git;
 public class ideaProject {
     public static void main(String[] args){
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
